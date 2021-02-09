4c70a419b68d6847a9b061dea0ee053028335b00
Saad100912 <arsaad.2016@gmail.com>
2021-02-09 12:30:40 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,109 @@
+// False values / Falsy -
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// True values / Truthy values -
+// "0"
+// " "
+// []
+// {}
+
+
+// const age = 4;
+// if(age){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// const name = " ";
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let name;
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let name = NaN;
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let name = NaN;
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let name = [];
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let name = {};
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// let name = false;
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+// let name = false;
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+let name = 1;
+if(name || name == 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
